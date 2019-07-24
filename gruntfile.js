105eb6f8d3d3294fae7d223dfa6f8af17b54e208
sachinmr <sachinmr96@outlook.com>
2019-07-24 15:09:59 +0530

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        express : {
+            dev: {
+                options: {
+                    port:3000,
+        script: 'app.js'
+      }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'public/stylesheets/style.css' : 'public/stylesheets/style.sass'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.sass',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.registerTask('dev',['express','sass','watch']);
+}
